b40efaaf7906a2543ce6c79b9d08c00dfa35db7d
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-11-11 11:19:47 -0600

    added button for create action.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <h1>hello</h1>
     <hr />
+    <button v-on:click="createPhotos()">Create Action</button>
     <div v-for="photo in photos">
       <p>ID:{{ photo.id }}</p>
       <p>Name:{{ photo.name }}</p>
@@ -29,11 +30,15 @@ created: function() {
 },
 methods: {
   indexPhotos: function() {
-console.log('hello')
 axios.get("http://localhost:3000/photos").then(response => {
   console.log(response.data)
   this.photos = response.data
-} )
+})
+},
+
+  createPhotos: function() {
+    console.log('hello')
+
 
   },
 
