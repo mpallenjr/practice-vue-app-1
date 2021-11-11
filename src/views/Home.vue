1db2101752b678a1afeb302609cc21f9ce0ea613
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-11-11 10:46:30 -0600

    connection between front and back visible in console. cors fixed, index not on webpage yet.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,23 +2,34 @@
   <div class="home">
     <div>
       <h1>hello</h1>
+      <p>{{ photos }}</p>
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
 data: function() {
 return {
   // where stuff goes in data
-
+photos: []
 };
 },
 created: function() {
+  this.indexPhotos();
 
 },
 methods: {
-  
+  indexPhotos: function() {
+console.log('hello')
+axios.get("http://localhost:3000/photos").then(response => {
+  console.log(response.data)
+} )
+
+  },
+
 },
 };
 
