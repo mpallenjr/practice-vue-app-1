2133bca64f97e9f613f662b95cc46a0f50c7daac
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-11-11 10:10:42 -0600

    initial commit, boilerplate home vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <h1>hello</h1>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+data: function() {
+return {
+  // where stuff goes in data
+
+};
+},
+created: function() {
+
+},
+methods: {
+  
+},
+};
+
 </script>
+
+<style>
+
+</style>
